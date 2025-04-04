78a2518e10d2d4c4c104590639d1c5efd41c3e81
Defilogist <defilogist@thewise.trade>
2025-04-04 14:05:45 +0200

    Add new full layout for the home page

---
+++ layouts/full.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col h-screen main">
+    <Navigation />
+    <div class="content" ref="contentDiv">
+      <div class="mx-auto p-0 bg-black shadow-md rounded-lg mt-8">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.main {
+  background-color: #000;
+  overflow-y: hidden;
+}
+
+.bg-black {
+  background-color: #000;
+  margin-bottom: 5em;
+  max-width: 1060px;
+}
+
+.content {
+  overflow-y: auto;
+}
+</style>
