5be1f6b88394288337671170f69499ca2e93f963
mooncitydev <hellomooncity@gmail.com>
2025-06-05 02:16:03 +0900

    feat: save the wallets

--- components/WalletAddress.vue
+++ components/WalletAddress.vue
@@ -94,6 +94,8 @@ const checkWalletAddress = async () => {
   const { solanaDomain, solanaAddress, wrongAddress, wrongDomain } =
     await validateWalletAddress(walletAddress.value)
   domainName.value = solanaDomain
+  console.log("🚀 ~ checkWalletAddress ~ solanaDomain:", solanaDomain)
+  
   errors.value.invalidAddress = wrongAddress
   errors.value.invalidDomain = wrongDomain
   if (
@@ -103,6 +105,7 @@ const checkWalletAddress = async () => {
     solanaAddress !== props.currentWalletAddress
   ) {
     console.log('walletAddressChanged', solanaAddress)
+    await storeAddress(solanaAddress, solanaDomain, "none");
     emitedWalletAddress = solanaAddress
     emit('walletAddressChanged', {
       address: solanaAddress,
