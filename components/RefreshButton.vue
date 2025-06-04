73500393aa6d4c53c24327ce562b99890a0d0807
Defilogist <defilogist@thewise.trade>
2025-06-04 09:20:26 +0200

    Add Refresh button component

---
+++ components/RefreshButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="button"
+    @click="emit('refresh')"
+  >
+    <ArrowPathIcon class="w-4 h-4" />
+  </button>
+</template>
+
+<script setup>
+import { ArrowPathIcon } from '@heroicons/vue/24/solid'
+const emit = defineEmits(['refresh'])
+</script>
+
+<style scoped>
+.button {
+  border: 1px solid #607CF6;
+  border-radius: 5px;
+  height: 36px;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
