768e5585756960d8def478270516d2a62dea8ba5
Defilogist <defilogist@thewise.trade>
2025-06-04 09:25:53 +0200

    Add placeholder for upcoming wallet page

---
+++ pages/dlmm-wallets.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Wallets</h1>
+    <p>coming soon...</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'app',
+})
+</script>
+
+<style scoped>
+
+</style>
