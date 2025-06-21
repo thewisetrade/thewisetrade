ebcbaa40fd0c26d6c7f094c10098b49c9efc7848
Defilogist <defilogist@thewise.trade>
2025-06-21 08:58:26 +0200

    Add Bin representation component

---
+++ components/positions/BinRepresentation.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="bin-wrapper">
+    <a
+      class="flex flex-row bin-container"
+      :href="`https://app.meteora.ag/dlmm/${props.positionKey}`"
+      target="_blank"
+    >
+      <div
+        :key="bin.id"
+        class="bin"
+        :style="binStyle(bin)"
+        v-for="bin in props.binData"
+      ></div>
+    </a>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  binData: {
+    type: Object,
+    required: true,
+  },
+  positionKey: {
+    type: String,
+    required: true,
+  },
+})
+
+const binStyle = (bin) => {
+  return {
+    backgroundColor: bin.positionXAmount > 0 ? '#6f61c0' : '#06aed4',
+  }
+}
+</script>
+
+<style scoped>
+.bin-wrapper {
+  width: 100%;
+}
+.bin-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+}
+.bin {
+  width: 3px;
+  height: 25px;
+  margin-right: 0px;
+}
+</style>
