6cf365818c464229313126aba12a0451f98e0762
Defilogist <defilogist@thewise.trade>
2025-10-18 23:18:53 +0200

    Improve bin representation

--- components/positions/BinRepresentation.vue
+++ components/positions/BinRepresentation.vue
@@ -5,18 +5,15 @@
       :href="`https://app.meteora.ag/dlmm/${props.positionKey}`"
       target="_blank"
     >
-      <div
-        :key="bin.id"
-        class="bin"
-        :style="binStyle(bin,index)"
-        v-for="(bin, index) in props.binData"
-      ></div>
+      <canvas ref="canvas" class="bin-canvas"></canvas>
     </a>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref, onMounted, watch } from 'vue'
+
+const canvas = ref(null)
 
 const props = defineProps({
   binData: {
@@ -37,7 +34,6 @@ const props = defineProps({
   },
 })
 
-console.log(props.positionToken1, props.positionToken2)
 
 const maxBinXAmount = computed(() => {
   const bins = props.binData || []
@@ -53,34 +49,61 @@ const maxBinYAmount = computed(() => {
   }, 0)
 })
 
-const maxBinAmount = computed(() => {
-  return Math.max(maxBinXAmount.value, maxBinYAmount.value)
-})
-
 const maxBinPrice = computed(() => {
   return Math.max(...props.binData.map(bin => bin.price))
 })
 
-const binStyle = (bin, index) => {
-  // console.log(bin)
-  // console.log(bin.positionXAmount, bin.positionYAmount, maxBinXAmount.value, maxBinYAmount.value, index)  )
-  console.log(maxBinPrice.value)
-  const styles = {
-    width: '3px',
-  }
-  if (props.binData.length > 140) {
-    styles.width = '2px'
-  }
-  if (bin.positionXAmount > 0) {
-    styles.backgroundColor = '#6f61c0'
-    styles.height = `${(1 - bin.price / maxBinPrice.value) * 25}px`
-  }
-  if (bin.positionYAmount > 0) {
-    styles.backgroundColor = '#06aed4'
-    styles.height = `${(1 -bin.price / maxBinPrice.value) * 25}px`
-  }
-  return styles
+const drawBins = () => {
+  if (!canvas.value) return
+
+  const ctx = canvas.value.getContext('2d')
+  const bins = props.binData || []
+
+  if (bins.length === 0) return
+
+  const binWidth = 2
+  const binMargin = 1
+  const maxHeight = 30
+
+  const canvasWidth = bins.length * (binWidth + binMargin)
+  const canvasHeight = maxHeight
+
+  canvas.value.width = canvasWidth
+  canvas.value.height = canvasHeight
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+  bins.forEach((bin, index) => {
+    console.log(bin.price, bin.positionXAmount, bin.positionYAmount, maxBinPrice.value)
+
+    const ratio = ((maxBinPrice.value - bin.price) / maxBinPrice.value) * 1.2
+    let height = ratio * maxHeight
+    let color = null
+
+    if (bin.positionXAmount > 0) {
+      color = '#6f61c0'
+    }
+    if (bin.positionYAmount > 0) {
+      color = '#06aed4'
+    }
+
+    if (color) {
+      const x = index * (binWidth + binMargin)
+      const y = canvasHeight - height
+
+      ctx.fillStyle = color
+      ctx.fillRect(x, y, binWidth, height)
+    }
+  })
 }
+
+onMounted(() => {
+  drawBins()
+})
+
+watch(() => props.binData, () => {
+  drawBins()
+}, { deep: true })
 </script>
 
 <style scoped>
@@ -94,10 +117,7 @@ const binStyle = (bin, index) => {
   justify-content: flex-start;
   width: 100%;
 }
-.bin {
-  width: 3px;
-  height: 25px;
-  margin-right: 0px;
-  padding: 0;
+.bin-canvas {
+  display: block;
 }
 </style>
